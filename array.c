83669fd3bee2b2448b595ea16cf78ba401274268
khuspure <71117356+khuspure@users.noreply.github.com>
2020-09-17 22:38:48 +0530

    Create array.c
    With the use of array we can print the "Matrix". 
    1  2  3
    4  5  6
    7  8  9

---
+++ array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+  int arr1[3][3],i,j;
+  
+       printf("The Input Elements In Matrix :\n");  
+
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+      	printf("element - [%d],[%d] : ",i,j);
+	      scanf("%d",&arr1[i][j]);
+      }
+  }  
+ 
+   printf("The Matrix Is : \n");
+  for(i=0;i<3;i++)
+  {
+       printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr1[i][j]);
+  }
+
+}
